c2b966da479a9b94c2c042691e7e2dd08671e1f7
Thomas Bye Nilsen <tni068@uit.no>
2020-01-08 13:06:33 +0100

    Tested the interface for gossiping between the Ifrit members

--- client/client.go
+++ client/client.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	//"github.com/joonnna/ifrit"
 	"ifrit"
-//	log "github.com/inconshreveable/log15"
+	log "github.com/inconshreveable/log15"
 //	"math/rand"
 	"time"
+	"errors"
+)
+
+var (
+	errCannotCreateApplication 		= errors.New("Cannot create application")
+	errCannotAddClients 			= errors.New("Adding one or more clients to Application.Clients failed")
 )
 
 type Application struct {
@@ -17,55 +23,95 @@ type Application struct {
 
 func main() {
 
-    // Enable line numbers in logging
-   // log.SetFlags(log.LstdFlags | log.Lshortfile)
-
-    // Will print: "[date] [time] [filename]:[line]: [text]"
- //   log.Println("Logging w/ line numbers on golangcode.com")
-
-	app, err := NewApplication()
+	nClients := 4
+	app, err := NewApplication(nClients)
 	if err != nil {
 		panic(err)
 	}
 
 	app.Start()
+	//app.RunGossipMessaging()
+	app.RunSimpleMessaging(nClients)
 }
 
-
-func NewApplication() (*Application, error) {
+func NewApplication(nClients int) (*Application, error) {
 	channel := make(chan bool, 1)
-	clients := make([]*ifrit.Client, 0)
-	
+
+	clients, err := AddClients(nClients)
+	if err != nil {
+		return nil, errCannotCreateApplication
+	}
+
 	app := &Application {
 		ExitChan: channel,
 	}
 
-	for i := 0; i < 8; i++ {
-		client, err := ifrit.NewClient()
-		if err != nil {
-			return nil, err
-		}
-	
-		clients = append(clients, client)
-		client.RegisterMsgHandler(app.simpleMessageHandler)
-		//fmt.Printf("Started node with IP = %s\n", client.Addr());
-		go client.Start()
-	}
-
 	app.Clients = clients
-
-	//fmt.Printf("len=%d cap=%d %v\n", len(clients), cap(clients), clients)
 	return app, nil
 }
 
-// This callback will be invoked on each received message.
-func (app *Application) simpleMessageHandler(data []byte) ([]byte, error) {
+func AddClients(nClients int) ([]*ifrit.Client, error) {
+	clients := make([]*ifrit.Client, 0)
 
-	return data, nil
+	for i := 0; i < nClients; i++ {
+		client, err := ifrit.NewClient()
+		if err != nil {
+			log.Error("Could not add client to application")
+			return nil, errCannotAddClients
+		}
+	
+		clients = append(clients, client)	
+	}
+
+	return clients, nil
 }
 
 func (app *Application) Start() {
-	
+	for _, client := range app.Clients {
+		go client.Start()
+		//client.RegisterGossipHandler(app.gossipMessageHandler)
+		//client.RegisterResponseHandler(app.gossipResponseHandler)
+		client.RegisterMsgHandler(app.simpleMessageHandler)
+	}
+}
+
+/** Gossip messaging here */
+func (app *Application) RunGossipMessaging() {
+	for {
+		select {
+
+			// TODO: exit main application loop
+			case <- app.ExitChan:
+				return
+
+			case <- time.After(time.Second * 0):
+				//randomClient := app.Clients[rand.Int() % len(app.Clients)]
+				s := fmt.Sprintf("Client %s sends message\n", app.Clients[0].Addr())
+				app.Clients[0].SetGossipContent([]byte(s))
+				
+				//ch := ap'p.Clients[0].SendTo(randomClient.Addr(), []byte(s))
+				//response := <- ch
+		}
+	}
+}
+
+func (app *Application) gossipMessageHandler(data []byte) ([]byte, error) {
+	fmt.Printf("In simpleGossipHandler() -- message: %s\n", string(data))
+	return data, nil
+}
+
+func (app *Application) gossipResponseHandler(data []byte) {
+	fmt.Printf("In gossipResponseHandler() -- message: %s\n", string(data))
+}	
+
+
+/****** simple message passing here ******/
+func (app *Application) simpleMessageHandler(data []byte) ([]byte, error) {
+	fmt.Printf("Message: %s\n", string(data));
+	return data, nil
+}
+
+func (app *Application) RunSimpleMessaging(nClients int) {
 	fmt.Printf("This node's address = %s\n", app.Clients[0].Addr());
 
 	for {
@@ -76,18 +122,19 @@ func (app *Application) Start() {
 			case <- time.After(time.Second * 1):
 
 				fmt.Printf("Sending 8 messages...\n")
-				for i := 0; i < 8; i++ {
+				for i := 1; i < nClients; i++ {
 					//idx := rand.Int() % len(app.Clients)
-					randomClient := app.Clients[i]
+					recipient := app.Clients[i]
 					//fmt.Printf("Sends message to %s\n", randomClient.Addr());
-					s := fmt.Sprintf("recipient: %s", randomClient.Addr())
-					ch := app.Clients[0].SendTo(randomClient.Addr(), []byte(s))
+					s := fmt.Sprintf("recipient: %s", recipient.Addr())
+					ch := app.Clients[0].SendTo(recipient.Addr(), []byte(s))
 				
 					response := <- ch
-					_ = response 
-					fmt.Printf("Message: %s\n", response)
+					fmt.Printf("%s\n", response)
 				}
 				fmt.Println()
 		}
 	}
 }
+
+
