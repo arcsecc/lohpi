e69c36dfbf453a61627f1b0de9b22fe8a2287776
Thomas Bye Nilsen <tni068@uit.no>
2020-01-15 16:31:16 +0100

    Added Firestore client structs, invoked ifrit clients and direct message passing

--- client/client.go
+++ client/client.go
@@ -2,139 +2,73 @@ package main
 
 import (
 	"fmt"
-	//"github.com/joonnna/ifrit"
 	"ifrit"
-	log "github.com/inconshreveable/log15"
-//	"math/rand"
-	"time"
 	"errors"
+	"encoding/gob"
+	"bytes"
+	"time"
 )
 
 var (
-	errCannotCreateApplication 		= errors.New("Cannot create application")
-	errCannotAddClients 			= errors.New("Adding one or more clients to Application.Clients failed")
+	errCreateRandomClientData 	= errors.New("Could not set client data.")
 )
 
-type Application struct {
-	Clients []*ifrit.Client
-	ExitChan chan bool
-	AppData string
+type Client struct {
+	Dataset map[string]*Clientdata		// string -> clientdata
+	ClientID string
+	IfritClient *ifrit.Client
 }
 
-func main() {
-
-	nClients := 4
-	app, err := NewApplication(nClients)
+func NewApplicationClient(clientID string) (*Client, error) {
+	ifritClient, err := ifrit.NewClient()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	app.Start()
-	//app.RunGossipMessaging()
-	app.RunSimpleMessaging(nClients)
+	client := &Client {
+		ClientID: clientID,
+		IfritClient: ifritClient,
+	}
+
+	ifritClient.RegisterMsgHandler(client.ClientMessageHandler)
+	return client, nil
 }
 
-func NewApplication(nClients int) (*Application, error) {
-	channel := make(chan bool, 1)
-
-	clients, err := AddClients(nClients)
-	if err != nil {
-		return nil, errCannotCreateApplication
+// Invoked when this client receives a message
+func (c *Client) ClientMessageHandler(data []byte) ([]byte, error) {
+	var decodedPayload Clientdata
+	b := bytes.NewBuffer(data)
+	d := gob.NewDecoder(b)
+	if err := d.Decode(&decodedPayload); err != nil {
+	   panic(err)
 	}
-
-	app := &Application {
-		ExitChan: channel,
-	}
-
-	app.Clients = clients
-	return app, nil
-}
-
-func AddClients(nClients int) ([]*ifrit.Client, error) {
-	clients := make([]*ifrit.Client, 0)
-
-	for i := 0; i < nClients; i++ {
-		client, err := ifrit.NewClient()
-		if err != nil {
-			log.Error("Could not add client to application")
-			return nil, errCannotAddClients
-		}
+  
+	startTime := decodedPayload.GetEpochTimestamp()
+	nSecInTransit := time.Now().UnixNano() - startTime
+	fmt.Printf("Message spent %d ns in transit\n", nSecInTransit)
 	
-		clients = append(clients, client)	
-	}
-
-	return clients, nil
+	return nil, nil
 }
 
-func (app *Application) Start() {
-	for _, client := range app.Clients {
-		go client.Start()
-		//client.RegisterGossipHandler(app.gossipMessageHandler)
-		//client.RegisterResponseHandler(app.gossipResponseHandler)
-		client.RegisterMsgHandler(app.simpleMessageHandler)
-	}
+func (c *Client) Start() {
+	go c.IfritClient.Start()
+	//fmt.Printf("addr = %s\n", c.IfritClient.Addr())
 }
 
-/** Gossip messaging here */
-func (app *Application) RunGossipMessaging() {
-	for {
-		select {
-
-			// TODO: exit main application loop
-			case <- app.ExitChan:
-				return
-
-			case <- time.After(time.Second * 0):
-				//randomClient := app.Clients[rand.Int() % len(app.Clients)]
-				s := fmt.Sprintf("Client %s sends message\n", app.Clients[0].Addr())
-				app.Clients[0].SetGossipContent([]byte(s))
-				
-				//ch := ap'p.Clients[0].SendTo(randomClient.Addr(), []byte(s))
-				//response := <- ch
-		}
-	}
+func (c *Client) ID() string {
+	return c.ClientID
 }
 
-func (app *Application) gossipMessageHandler(data []byte) ([]byte, error) {
-	fmt.Printf("In simpleGossipHandler() -- message: %s\n", string(data))
-	return data, nil
+// Use this to access the underlying Ifrit client. Should find a more sane way to 
+// separate an ifrit client and an ordinary client...
+func (c *Client) FireflyClient() *ifrit.Client {
+	return c.IfritClient
 }
 
-func (app *Application) gossipResponseHandler(data []byte) {
-	fmt.Printf("In gossipResponseHandler() -- message: %s\n", string(data))
-}	
-
-
-/****** simple message passing here ******/
-func (app *Application) simpleMessageHandler(data []byte) ([]byte, error) {
-	fmt.Printf("Message: %s\n", string(data));
-	return data, nil
+func (c *Client) ClientData(key string) *Clientdata {
+	return c.Dataset[key]
 }
 
-func (app *Application) RunSimpleMessaging(nClients int) {
-	fmt.Printf("This node's address = %s\n", app.Clients[0].Addr());
-
-	for {
-		select {
-			case <- app.ExitChan:
-				return
-
-			case <- time.After(time.Second * 1):
-
-				fmt.Printf("Sending 8 messages...\n")
-				for i := 1; i < nClients; i++ {
-					//idx := rand.Int() % len(app.Clients)
-					recipient := app.Clients[i]
-					//fmt.Printf("Sends message to %s\n", randomClient.Addr());
-					s := fmt.Sprintf("recipient: %s", recipient.Addr())
-					ch := app.Clients[0].SendTo(recipient.Addr(), []byte(s))
-				
-					response := <- ch
-					fmt.Printf("%s\n", response)
-				}
-				fmt.Println()
-		}
-	}
+func (c *Client) InsertClientdata(clientData *Clientdata) {
+	c.Dataset[clientData.BlobHash] = clientData
 }
-
-
