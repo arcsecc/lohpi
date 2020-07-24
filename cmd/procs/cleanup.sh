1c8f1334657d1991732486f3613e46cdca4fab07
Thomas Bye Nilsen <thomasbn94@gmail.com>
2020-07-24 15:02:03 +0200

    added cleanup script and backlog

---
+++ cmd/procs/cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Cleaning up resources"
+pidof procs | xargs kill -9 2> /dev/zero 
+pidof node | xargs kill -9 2> /dev/zero
+
+# Number of nodes
+for i in {0..100}
+do
+   fusermount -u /tmp/storage_nodes/node_$i 2> /dev/zero
+done
+
+# Remove FUSE points
+rm -rf /home/thomas/go/src/github.com/tomcat-bit/lohpi/storage_nodes/ /tmp/storage_nodes
+rm *logfile
+rm -rf /home/thomas/go/src/github.com/tomcat-bit/lohpi/policy_store
+mkdir /home/thomas/go/src/github.com/tomcat-bit/lohpi/policy_store
+git init /home/thomas/go/src/github.com/tomcat-bit/lohpi/policy_store
