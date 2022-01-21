73c5b904eb88c46c1fb8677127b2b26b6bfcd89b
Thomas Bye Nilsen <thomasbn94@gmail.com>
2022-01-21 18:36:26 +0100

    making room for Let's Encrypt

--- core/comm/local_crypto_unit.go
+++ core/comm/local_crypto_unit.go
@@ -8,16 +8,12 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/json"
-	"encoding/pem"
 	"errors"
 	"fmt"
-	log "github.com/sirupsen/logrus"
-	"io/ioutil"
 	"math/big"
 	"net"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 	"time"
 )
@@ -26,12 +22,12 @@ var (
 	errNoIp               = errors.New("No ip present in received identity")
 	errNoAddrs            = errors.New("Not enough addresses present in identity")
 	errNoCa               = errors.New("No Lohpi ca address supplied")
-	errNoCryptoUnitConfig = errors.New("No crypto unit config present")
+	errNoLocalCryptoUnitConfig = errors.New("No crypto unit config present")
 	errNoPath             = errors.New("Crypto unit path is not set")
 	errNoHostnames        = errors.New("Hostnames is not set")
 )
 
-type CryptoUnit struct {
+type LocalCryptoUnit struct {
 	priv   *ecdsa.PrivateKey
 	caAddr string
 
@@ -54,13 +50,13 @@ type certSet struct {
 	caCert  *x509.Certificate
 }
 
-type CryptoUnitConfig struct {
+type LocalCryptoUnitConfig struct {
 	Identity  pkix.Name
 	CaAddr    string
 	Hostnames []string
 }
 
-func NewCu(path string, config *CryptoUnitConfig) (*CryptoUnit, error) {
+func NewLocalCryptoUnit(path string, config *LocalCryptoUnitConfig) (*LocalCryptoUnit, error) {
 	if path == "" {
 		return nil, errNoPath
 	}
@@ -86,7 +82,7 @@ func NewCu(path string, config *CryptoUnitConfig) (*CryptoUnit, error) {
 		return nil, err
 	}
 
-	return &CryptoUnit{
+	return &LocalCryptoUnit{
 		ca: certs.caCert,
 		self: certs.ownCert,
 		priv: priv,
@@ -97,9 +93,9 @@ func NewCu(path string, config *CryptoUnitConfig) (*CryptoUnit, error) {
 	}, nil
 }
 
-func validateConfiguration(config *CryptoUnitConfig) error {
+func validateConfiguration(config *LocalCryptoUnitConfig) error {
 	if config == nil {
-		return errNoCryptoUnitConfig
+		return errNoLocalCryptoUnitConfig
 	}
 
 	if len(config.Hostnames) == 0 {
@@ -113,135 +109,19 @@ func validateConfiguration(config *CryptoUnitConfig) error {
 	return nil
 }
 
-func LoadCu(path string) (*CryptoUnit, error) {
-	// Load own cert
-	certPath := filepath.Join(path, "owncert.pem")
-	fp, err := ioutil.ReadFile(certPath)
-	if err != nil {
-		return nil, err
-	}
-
-	certBlock, _ := pem.Decode(fp)
-	ownCert, err := x509.ParseCertificate(certBlock.Bytes)
-	if err != nil {
-		return nil, err
-	}
-
-	// Load CA's cert
-	certPath = filepath.Join(path, "cacert.pem")
-	fp, err = ioutil.ReadFile(certPath)
-	if err != nil {
-		return nil, err
-	}
-
-	certBlock, _ = pem.Decode(fp)
-	caCert, err := x509.ParseCertificate(certBlock.Bytes)
-	if err != nil {
-		return nil, err
-	}
-
-	// Load private key
-	keyPath := filepath.Join(path, "key.pem")
-	fp, err = ioutil.ReadFile(keyPath)
-	if err != nil {
-		return nil, err
-	}
-
-	keyBlock, _ := pem.Decode(fp)
-	key, err := x509.ParseECPrivateKey(keyBlock.Bytes)
-	if err != nil {
-		return nil, err
-	}
-
-	return &CryptoUnit{
-		ca:               caCert,
-		self:             ownCert,
-		priv:             key,
-		keyFilePath:      "key.pem",
-		selfCertFilePath: "owncert.pem",
-		caCertFilePath:   "cacert.pem",
-		workingDirectory: path,
-		//caAddr?
-	}, nil
-}
-
-func (cu *CryptoUnit) SaveState() error {
-	// Save private key
-	p := filepath.Join(cu.workingDirectory, cu.keyFilePath)
-	f, err := os.Create(p)
-	if err != nil {
-		log.Errorln(err.Error())
-	}
-
-	b, err := x509.MarshalECPrivateKey(cu.priv)
-	if err != nil {
-		log.Errorln(err.Error())
-	}
-
-	block := &pem.Block{
-		Type:  "EC PRIVATE KEY",
-		Bytes: b,
-	}
-
-	if err := pem.Encode(f, block); err != nil {
-		log.Errorln(err.Error())
-	}
-
-	// Save CA's X.509 certificate
-	p = filepath.Join(cu.workingDirectory, cu.caCertFilePath)
-	f, err = os.Create(p)
-	if err != nil {
-		log.Errorln(err.Error())
-	}
-
-	// Save CA cert
-	b = cu.ca.Raw
-	block = &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: b,
-	}
-
-	if err := pem.Encode(f, block); err != nil {
-		log.Errorln(err.Error())
-	}
-
-	if err := f.Close(); err != nil {
-		log.Errorln(err.Error())
-	}
-
-	// Save my own X.509 certificate
-	p = filepath.Join(cu.workingDirectory, cu.selfCertFilePath)
-	f, err = os.Create(p)
-	if err != nil {
-		log.Errorln(err.Error())
-	}
-
-	b = cu.self.Raw
-	block = &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: b,
-	}
-
-	if err := pem.Encode(f, block); err != nil {
-		log.Errorln(err.Error())
-	}
-
-	return f.Close()
-}
-
-func (cu *CryptoUnit) Certificate() *x509.Certificate {
+func (cu *LocalCryptoUnit) Certificate() *x509.Certificate {
 	return cu.self
 }
 
-func (cu *CryptoUnit) CaCertificate() *x509.Certificate {
+func (cu *LocalCryptoUnit) CaCertificate() *x509.Certificate {
 	return cu.ca
 }
 
-func (cu *CryptoUnit) PrivateKey() *ecdsa.PrivateKey {
+func (cu *LocalCryptoUnit) PrivateKey() *ecdsa.PrivateKey {
 	return cu.priv
 }
 
-func (cu *CryptoUnit) PublicKey() *ecdsa.PublicKey {
+func (cu *LocalCryptoUnit) PublicKey() *ecdsa.PublicKey {
 	return &cu.priv.PublicKey
 }
 
@@ -255,8 +135,8 @@ func sendCertRequest(privKey *ecdsa.PrivateKey, caAddr string, pk pkix.Name, hos
 
 	template := x509.CertificateRequest{
 		SignatureAlgorithm: x509.ECDSAWithSHA256,
-		Subject:            pk,
-		DNSNames:           hostnames,
+		Subject: pk,
+		DNSNames: hostnames,
 	}
 
 	certReqBytes, err := x509.CreateCertificateRequest(rand.Reader, &template, privKey)
@@ -288,6 +168,7 @@ func sendCertRequest(privKey *ecdsa.PrivateKey, caAddr string, pk pkix.Name, hos
 	return set, nil
 }
 
+// TODO: enable me someday...
 func selfSignedCert(priv *ecdsa.PrivateKey, pk pkix.Name) (*certSet, error) {
 	ext := pkix.Extension{
 		Id:       []int{2, 5, 13, 37},
